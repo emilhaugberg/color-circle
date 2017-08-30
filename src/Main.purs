0719f1f203630fa38bcb66d39b57e767ee8f11b1
Emil Haugberg <emilveen@gmail.com>
2017-08-30 22:54:25 +0200

    Update dependencies.

--- src/Main.purs
+++ src/Main.purs
@@ -3,6 +3,9 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Graphics.Canvas
+
+
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
