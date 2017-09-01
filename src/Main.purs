95d7d2066c134f75c13279a3fc4c2a9dd05d0f6b
Emil Haugberg <emilveen@gmail.com>
2017-09-01 14:01:32 +0200

    Code cleanup.

--- src/Main.purs
+++ src/Main.purs
@@ -10,31 +10,33 @@ import Math as Math
 
 type Radius   = Number
 type Color    = String
+type Angle    = Number
 type Position = { x   :: Number,   y      :: Number }
 type Circle   = { pos :: Position, radius :: Radius }
 
+-- dimensions of canvas
 width  = 600.0
 height = 600.0
 
+-- radius and center of main circle
 radius = 250.0
-
 centerX = width  / 2.0
 centerY = height / 2.0
 
-type Angle = Number
-
-nums :: Array Number
-nums = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0,20.0,21.0,22.0,23.0,24.0]
-
-posToAng :: Number -> Angle
-posToAng n = (/) (Math.pi * 2.0) n
+circlePositions :: Array Number
+circlePositions =
+  [  1.0,  2.0,  3.0,  4.0,  5.0,  6.0,  7.0,  8.0,  9.0,  10.0, 11.0, 12.0
+  , 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0,  23.0, 24.0]
 
 angToPos :: Angle -> Position
 angToPos ang = {x: ((Math.cos ang) * radius) + 300.0, y: ((Math.sin ang) * radius) + 300.0}
 
+-- space between circles along circle
+space :: Number
 space = Math.pi * 2.0 / 24.0
 
-angles = map ((*) space) nums
+angles :: Array Angle
+angles = map ((*) space) circlePositions
 
 drawCircle :: forall e. Circle -> Color -> Context2D -> Eff (canvas :: CANVAS | e) Context2D
 drawCircle cir col ctx = do
@@ -48,8 +50,8 @@ main :: forall e. (Partial) => Eff (canvas :: CANVAS, console :: CONSOLE | e) Un
 main = do
   Just canvas <- getCanvasElementById "circle"
   ctx         <- getContext2D canvas
-  _           <- drawCircle { pos: { x: 300.0, y: 300.0 }, radius: 250.0 } "#F44336" ctx
+  _           <- drawCircle { pos: { x: 300.0, y: 300.0 }, radius: 250.0 } "#fff" ctx
   foreachE angles \n -> void do
     let pos = angToPos n
-    let cir = {pos: pos, radius: 10.0}
+    let cir = { pos: pos, radius: 20.0 }
     drawCircle cir "black" ctx
